f9a4f1db075870c664ce0dbb2ebb64e71898194a
oyinlola101 <rustyoyinlola@gmail.com>
2024-03-17 18:50:23 +0100

    this is my first commit

---
+++ terraform/modules/argocd/provider.tf
@@ -0,0 +1,43 @@
+provider "google" {
+  credentials = file("C:/Users/oyin/Desktop/practice/service.json")
+  project     = var.project_id
+  region      = var.region
+}
+
+#data "google_client_config" "default" {}
+
+
+
+data "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+  project  = var.project_id
+}
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${var.kubernetes_cluster_host}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${var.kubernetes_cluster_host}"
+   token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "argo" {
+  name       = "argocd"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace = "argocd"
+  create_namespace = true
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+}
---
+++ terraform/modules/argocd/vars.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+variable "kubernetes_cluster_host" {
+  description = "clusterhost"
+}
